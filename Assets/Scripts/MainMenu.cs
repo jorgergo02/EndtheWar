e7b898be445effa5f939bcab0878637c7d486e07
jorgergo02 <A01367845@tec.mx>
2022-04-25 19:46:05 -0500

    updates

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -11,10 +11,28 @@ public class MainMenu : MonoBehaviour
    }
     public static MainMenu instance;
     public string startScene;
+    public string level1;
+    public string level2;
+    public string level3;
 
     public void StartGame()
     {
         SceneManager.LoadScene(startScene);
+    }
+
+    public void firstLevel()
+    {
+        SceneManager.LoadScene(level1);
+    }
+
+    public void secondLevel()
+    {
+        SceneManager.LoadScene(level2);
+    }
+    
+    public void thirdLevel()
+    {
+        SceneManager.LoadScene(level3);
     }
 
     public void QuitGame()
